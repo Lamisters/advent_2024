e19a7f5d86801c2b078723e47c93f45b5d5c2cc2
ben arsenault <arsenaultb@gmail.com>
2024-12-01 12:14:21 -0400

    initial creation of project structure

---
+++ solutions/main.py
@@ -0,0 +1,6 @@
+from puzzle_loader import read_puzzle_input
+
+if __name__ == '__main__':
+    input = read_puzzle_input('example_input.dat')
+
+    print(input)
---
+++ solutions/puzzle_loader.py
@@ -0,0 +1,10 @@
+import os
+
+def read_puzzle_input(file_name: str) -> str:
+    ''' Reads the puzzle input data from the given file and returns the entire
+    contents as a string. '''
+
+    file_path = os.path.join('data',file_name)
+
+    with open(file_path, 'r') as f:
+        return f.read()

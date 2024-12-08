f91051815acb5849e93824316e753cf002b2e520
Ben <arsenaultb@gmail.com>
2024-12-08 18:30:19 -0400

    started day 5

---
+++ solutions/day_5.py
@@ -0,0 +1,10 @@
+from puzzle_reader import read_puzzle
+
+def solve_part_one():
+    ''' Solve part one of the puzzle. '''
+    puzzle_input = read_puzzle('example_input.dat')
+
+    print(puzzle_input)
+
+if __name__ == '__main__':
+    solve_part_one()
